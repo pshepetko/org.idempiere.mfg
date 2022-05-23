79fac0c6d6f4163be38f9857aa26358c2f504b93
Peter Shepetko <pshepetko@gmail.com>
2022-05-23 12:32:30 +0300

    Add files via upload
    Classname update for forms and processes

---
+++ downloads/update_classname.sql
@@ -0,0 +1,7 @@
+ UPDATE AD_Process 
+ SET classname ='org.libero'||SUBSTRING(classname,15,70)
+ WHERE SUBSTRING(classname,0,15) = 'org.eevolution';
+
+ UPDATE AD_Form 
+ SET classname ='org.libero'||SUBSTRING(classname,15,70)
+ WHERE SUBSTRING(classname,0,15) = 'org.eevolution';
