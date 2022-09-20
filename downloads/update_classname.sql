607d53a82f753f0b21ba591606e63569210e9a28
Peter Shepetko <pshepetko@gmail.com>
2022-09-20 19:20:42 +0300

    Update update_classname.sql

--- downloads/update_classname.sql
+++ downloads/update_classname.sql
@@ -6,5 +6,5 @@
  SET classname ='org.libero'||SUBSTRING(classname,15,70)
  WHERE SUBSTRING(classname,0,15) = 'org.eevolution';
  
- UPDATE AD_Column  SET callout =null
+ UPDATE AD_Column  SET callout = null
  WHERE SUBSTRING(callout,0,21) = 'org.eevolution.model';
